5431b9dd723fb4e90901d76138c4799c4376827d
donghyuk <161474075+fbo1224@users.noreply.github.com>
2025-11-26 18:30:15 +0900

    수상작13

--- src/main/resources/static/js/award.js
+++ src/main/resources/static/js/award.js
@@ -31,6 +31,11 @@ $('.reg-list-row').on('click', function() {
          console.error(error);
       }
    });
+});
+
+// 이미지 다운로드
+$('.download-btn').on('click', function() {
+   console.log('download');
 
 
 });
@@ -190,6 +195,54 @@ $('.del-btn').on('click', function() {
         alert('선택된 회원이 없습니다.');
     }
 });
+
+// 우편번호 API
+function sample4_execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            var roadAddr = data.roadAddress; // 도로명 주소 변수
+            var extraRoadAddr = ''; // 참고 항목 변수
+            if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                extraRoadAddr += data.bname;
+            }
+            if(data.buildingName !== '' && data.apartment === 'Y'){
+                extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+            }
+            if(extraRoadAddr !== ''){
+                extraRoadAddr = ' (' + extraRoadAddr + ')';
+            }
+
+            document.getElementById('sample4_postcode').value = data.zonecode;
+            document.getElementById("sample4_roadAddress").value = roadAddr;
+
+            if(roadAddr !== ''){
+                document.getElementById("sample4_extraAddress").value = extraRoadAddr;
+            } else {
+                document.getElementById("sample4_extraAddress").value = '';
+            }
+
+            var guideTextBox = document.getElementById("guide");
+            if(data.autoRoadAddress) {
+                var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+                guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+                guideTextBox.style.display = 'block';
+
+            } else if(data.autoJibunAddress) {
+                var expJibunAddr = data.autoJibunAddress;
+                guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+                guideTextBox.style.display = 'block';
+            } else {
+                guideTextBox.innerHTML = '';
+                guideTextBox.style.display = 'none';
+            }
+        }
+    }).open();
+}
+
+$('.post-btn').on('click', function() {
+    sample4_execDaumPostcode();
+});
+
 
 // 수정 버튼 클릭
 $('.mod-btn').on('click', function() {
--- src/main/resources/static/js/similar.js
+++ src/main/resources/static/js/similar.js
@@ -1,85 +1,79 @@
 $(document).ready(function () {
+    const dropArea = $('.upload-box');
+    const fileInput = $('#fileInput');
+    const statusText = $('.status-text');
+    const allowedExt = ['png', 'jpg', 'jpeg'];
+
+    function handleFile(file) {
+        const ext = file.name.split('.').pop().toLowerCase();
+
+        // 실패 처리
+        if (!allowedExt.includes(ext)) {
+            $('.fileImg').attr('src', '/images/file_error.png');
+            $('.upload-text').hide();
+            $('.status-text').text('파일 업로드 실패').css('color','red').show();
+            return;
+        }
+
+        // 성공 처리
+        $('.fileImg').attr('src', '/images/file.png');
+        $('.upload-text').hide();
+        $('.status-text').text('파일 업로드 성공').css('color','green').show();
+
+        // 업로드 영역 업데이트
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // 이미지 미리보기
+            $('.img-div .upload-img').attr('src', e.target.result);
+
+            // 파일 정보 텍스트
+            const fileSizeKB = (file.size / 1024).toFixed(1); // kb 단위
+            const img = new Image();
+            img.onload = function() {
+                const width = img.width;
+                const height = img.height;
+                const infoText = `파일명 : ${file.name}\n이미지 사이즈: ${width}x${height}\n이미지 크기: ${fileSizeKB}kb`;
+                $('.text-div pre').text(infoText);
+            };
+            img.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+
+    // 클릭 시 파일 선택
+    dropArea.on('click', function() {
+        fileInput.click();
+    });
+
+    fileInput.on('click', function(e){
+        e.stopPropagation(); // 무한 호출 방지
+    });
+
+    fileInput.on('change', function() {
+        if (this.files.length > 0) handleFile(this.files[0]);
+    });
+
+    // 드래그 이벤트
+    dropArea.on('dragover', function(e) {
+        e.preventDefault();
+        e.originalEvent.dataTransfer.dropEffect = 'copy';
+        dropArea.addClass('drag-over');
+    });
+
+    dropArea.on('dragleave dragend', function(e) {
+        dropArea.removeClass('drag-over');
+    });
 
     dropArea.on('drop', function(e) {
         e.preventDefault();
-        const files = e.originalEvent.dataTransfer.files;
-        if (files.length > 0) {
-            const file = files[0];
-            const ext = file.name.split('.').pop().toLowerCase();
-
-            if (!allowedExt.includes(ext)) {
-                $('.fileImg').attr('src', '/assets/fileUpload_Error.png');
-                $('.modal-back-1 p').css('display', 'none');
-                $('.modal-back-2 p').css('display', 'none');
-
-                return; // 업로드 중단
-            }
-            uploadFile(file);
-        }
-    });
-
-    $('#fileInput').on('change', function() {
-        const file = this.files[0];
-        if (file) {
-            const ext = file.name.split('.').pop().toLowerCase();
-            if (!allowedExt.includes(ext)) {
-                $('.fileImg').attr('src', '/assets/fileUpload_Error.png');
-                $('.modal-back-1 p').css('display', 'none');
-                $('.modal-back-2 p').css('display', 'none');
-                return;
-            }
-            uploadFile(file);
+        dropArea.removeClass('drag-over');
+        if (e.originalEvent.dataTransfer.files.length > 0) {
+            handleFile(e.originalEvent.dataTransfer.files[0]);
         }
     });
 });
 
-
-function uploadFile(file) {
-    const img = $('.fileImg');
-    const p = $('.modal-back-1 p');
-    const p2 = $('.modal-back-2 p');
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    $.ajax({
-        url: '/Employee/upload_excel',
-        type: 'post',
-        data: formData,
-        dataType: 'json',
-        contentType: false,
-        processData: false,
-        success: function(response) {
-            // console.log('응답:', response);
-
-            if (response['status']) {
-
-                alert(response['msg']);
-
-                img.attr('src', '/assets/fileIcon.png');
-                text.text(file.name);
-
-                p.css('display', 'none');
-                p2.css('display', 'none');
-            } else {
-                alert(response['msg']);
-
-                p.css('display', 'none');
-                p2.css('display', 'none');
-
-                img.attr('src', '/assets/fileUpload_Error.png');
-            }
-        },
-        error: function(error) {
-            p.css('display', 'none');
-            p2.css('display', 'none');
-
-            // console.log('에러:', error);
-            img.attr('src', '/assets/fileUpload_Error.png');
-        }
-    });
-}
-
+// image modal
 $('.img-div').on('click', function() {
     $('.modal-img').attr('src', $(this).find('img').attr('src'));
     $('#myModal').fadeIn();
