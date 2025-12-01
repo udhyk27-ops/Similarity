dae02880e95e2cd989a3e94bff65f1f52300d4c1
LNY <limny0831@naver.com>
2025-12-01 15:17:14 +0900

    251201 AWARD dto 필드추가
    f_year, f_manage, f_host, f_filepath 추가

--- src/main/java/com/example/java_practice/commons/dto/Award.java
+++ src/main/java/com/example/java_practice/commons/dto/Award.java
@@ -7,8 +7,12 @@ public class Award {
     private int f_work_no;
     private String f_title;
     private String f_author;
+    private String f_year;
     private String f_contest;
     private String f_award;
     private String f_code;
+    private String f_manager;
+    private String f_host;
     private String f_reg_date;
+    private String f_filepath;
 }
